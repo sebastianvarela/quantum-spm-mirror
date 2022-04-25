a15aca83f1f75fcd96166ed8017c6fefc6fde85b
Derek Corley <dcorley@quantummetric.com>
2022-04-25 12:02:32 -0600

    Matching target and artifact names to support 13.3

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "QMNative",
-            targets: ["QMNative"]),
+            name: "libQMNative",
+            targets: ["libQMNative"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "QMNative",
+            name: "libQMNative",
             path: "Sources/libQMNative.xcframework"
         )
     ]
